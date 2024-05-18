01a3b782a1d7b36018ebc0b4913813150c23b583
PinyaGOOO <165675370+PinyaGOOO@users.noreply.github.com>
2024-05-18 12:51:49 +0300

    Add files via upload

--- br-srv.sh
+++ br-srv.sh
@@ -3,9 +3,6 @@ dnf remove -y git
 nmcli con modify ens18 ipv4.method manual ipv4.addresses 192.168.100.10/28
 nmcli con modify ens18 ipv4.gateway 192.168.100.1
 
-nmcli con modify ens18 ipv6.method manual ipv6.addresses FD24:192::10/124
-nmcli con modify ens18 ipv6.gateway FD24:192::1
-
 useradd -c "Branch admin" Branch_admin -U
 echo "Branch_admin:P@ssw0rd" | chpasswd
 useradd -c "Network Admin" Network_admin -U
