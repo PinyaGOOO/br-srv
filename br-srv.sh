be5fbc72894c5fdc955f4b5bd362d22bfc714c5b
PinyaGOOO <165675370+PinyaGOOO@users.noreply.github.com>
2024-05-29 20:00:33 +0300

    Add files via upload

--- br-srv.sh
+++ br-srv.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 dnf remove -y git
+dnf install -y chrony
 nmcli con modify ens18 ipv4.method manual ipv4.addresses 192.168.100.10/28
 nmcli con modify ens18 ipv4.gateway 192.168.100.1
 
@@ -9,6 +10,10 @@ sed -i '4s/^/#/' /etc/chrony.conf
 sed -i '5s/^/#/' /etc/chrony.conf
 sed -i '6s/^/#/' /etc/chrony.conf
 sed -i '7a\server 172.16.100.1 iburst prefer' /etc/chrony.conf
+
+systemctl enable --now chronyd
+systemctl restart chronyd
+chronyc sources
 
 useradd -c "Branch admin" Branch_admin -U
 echo "Branch_admin:P@ssw0rd" | chpasswd
